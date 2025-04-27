8def9358c36bba91f2fa7ec9312896c44389f77e
wilsonz3-dev <205842906+wilsonz3-dev@users.noreply.github.com>
2025-04-27 18:34:40 -0400

    add quiz submission logic

--- backend/routes/quiz.py
+++ backend/routes/quiz.py
@@ -12,5 +12,25 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 @quiz_route.route("/submit", methods=["POST"])
 def submit_quiz():
-    # Logic for quiz submission
-    return None
+    data = request.get_json()
+    
+    # Fields from frontend api call
+    user_id = data.get("user_id")
+    quiz_id = data.get("quiz_id")
+    points = data.get("points")
+    
+    if not all([user_id, quiz_id, points]):
+        return jsonify({"error": "Missing Fields"}), 400
+    
+    try:
+        result = supabase.table("scores").insert({
+            "id": str(uuid.uuid4()),
+            "user_id": user_id,
+            "quiz_id": quiz_id,
+            "points": points
+        }).execute()
+        
+        return jsonify({"message": "Quiz submission recorded", "data": result.data}), 200
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
