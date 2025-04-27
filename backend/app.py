50ccf104ed789d0360d57c7f909c592b7e4a9505
wilsonz3-dev <205842906+wilsonz3-dev@users.noreply.github.com>
2025-04-27 13:42:03 -0400

    set up flask backend

--- backend/app.py
+++ backend/app.py
@@ -1,38 +1,22 @@
-from flask import Flask, request, jsonify
-from supabase import create_client, Client
-from dotenv import load_dotenv
+from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
 import os
+
+from routes.auth import auth_route
+from routes.quiz import quiz_route
+from routes.leaderboard import leaderboard_route
 
 load_dotenv()
 
+# Initialize flask app & set up CORS
 app = Flask(__name__)
 CORS(app)
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-@app.route("/verify", methods=["POST"])
-def verify():
-    """User verification using OAuth"""
-    auth_header = request.headers.get("Authorization", "") # Get the header
-    if not auth_header.startswith("Bearer "):
-        return jsonify({"error": "Missing token"}), 401
-
-    token = auth_header.split("Bearer ")[-1] # Extract the token
-    try:
-        user = supabase.auth.get_user(token).user
-        if user:
-            user_info = {
-                "email": user.email,
-                "name": user.user_metadata.get("full_name", "Unknown"),
-            }
-            return jsonify({"info": user_info})
-        else:
-            return jsonify({"error": "User not found"}), 404
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+# Register Blueprints 
+app.register_blueprint(auth_route, url_prefix="/auth")
+app.register_blueprint(quiz_route, url_prefix="/quiz")
+app.register_blueprint(leaderboard_route, url_prefix="/leaderboard")
 
 @app.route("/")
 def home():
