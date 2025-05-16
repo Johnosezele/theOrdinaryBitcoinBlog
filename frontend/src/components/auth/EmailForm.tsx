cfa93e4fdfdcb5ede5b8c518a000c3e1c1ee5bff
Johnosezele <johnosezele@gmail.com>
2025-05-16 01:24:48 +0100

    deployed to vercel

--- frontend/src/components/auth/EmailForm.tsx
+++ frontend/src/components/auth/EmailForm.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useAuth } from '../../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
 
 interface EmailFormProps {
   mode: 'login' | 'signup';
